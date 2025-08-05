8b6e588309d1551f6fb8e46042a288b0b6490de7
ASteinheiser <me@iamandrew.io>
2025-08-05 15:53:15 -0700

    no need for await

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,21 @@ import fs from 'fs';
 import { snootyAstToMdast } from './snooty-ast-to-mdast';
 import { mdastToMdx } from './mdast-to-mdx';
 
-const main = async () => {
-  const [_, __, input] = process.argv;
+const [_, __, input] = process.argv;
 
-  if (!input) {
-    console.error('Usage: pnpm start /path/to/ast-input.json');
-    process.exit(1);
-  }
+if (!input) {
+  console.error('Usage: pnpm start /path/to/ast-input.json');
+  process.exit(1);
+}
 
-  const raw = JSON.parse(fs.readFileSync(input, 'utf8'));
-  // handle wrapper objects that store AST under `ast` field
-  const snootyRoot = raw.ast ?? raw;
+const raw = JSON.parse(fs.readFileSync(input, 'utf8'));
+// handle wrapper objects that store AST under `ast` field
+const snootyRoot = raw.ast ?? raw;
 
-  const mdast = snootyAstToMdast(snootyRoot);
-  const mdx = await mdastToMdx(mdast);
+const mdast = snootyAstToMdast(snootyRoot);
+const mdx = mdastToMdx(mdast);
 
-  const outputPath = input.replace('ast-input.json', 'output.mdx');
-  fs.writeFileSync(outputPath, mdx);
+const outputPath = input.replace('ast-input.json', 'output.mdx');
+fs.writeFileSync(outputPath, mdx);
 
-  console.log(`Output written to ${outputPath}`);
-};
-
-main();
+console.log(`Output written to ${outputPath}`);
